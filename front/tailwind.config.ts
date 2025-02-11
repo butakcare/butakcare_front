0ddec2ec818bb3009198ad265320673bba3a6502
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-02-12 01:11:45 +0900

    style: 색상 테마 확장

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -11,12 +11,22 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        key: "#65CCB2",
+        sub: "#B2E2CC",
+        white: "#FFFFFF",
+        gray10: "#FCFCFB",
+        gray20: "#F6F4ED",
+        gray30: "#F0EEE5",
+        gray90: "#3B3B3B",
+        black: "#000000",
+
       },
       screens: {
         mobile: "360px",
         tablet: "768px",
         desktop: "1100px",
       },
+      
     },
   },
   plugins: [],
