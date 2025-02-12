6501e4e578a2f8c605c5e7131a20a8cc794717b4
이종원 <jongwon@ijong-won-ui-MacBookPro.local>
2025-02-12 12:16:30 +0900

    feat: 요양보호사 나의 매칭 페이지 완성

--- front/src/app/guardian/main/page.tsx
+++ front/src/app/guardian/main/page.tsx
@@ -1,13 +1,17 @@
 import Header from "@/components/guardian/main/Header";
 import Matching from "@/components/guardian/main/Matching";
 import MyMatching from "@/components/guardian/main/MyMatching";
+import MySchedule from "@/components/guardian/main/MySchedule";
 
 export default function Home() {
   return (
     <div className="w-screen h-screen max-tablet:flex max-tablet:flex-col max-tablet:items-center">
-      <Header />
+      <div className="max-tablet:h-[153px]">
+        <Header />
+      </div>
       <Matching />
       <MyMatching />
+      <MySchedule />
     </div>
   );
 }
---
+++ front/src/components/guardian/main/MySchedule.tsx
@@ -0,0 +1,9 @@
+export default function MySchedule() {
+  return (
+    <div className="max-tablet:w-[353px] max-tablet:mt-[47px]">
+      <strong className="max-tablet:block max-tablet:text-[26px] max-tablet:font-[700] max-tablet:text-[#000000] max-tablet:leading-[31px]">
+        나의 근무 일정
+      </strong>
+    </div>
+  );
+}
